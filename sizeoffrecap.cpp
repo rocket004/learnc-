f4d48a9e0db705f35beac12da36fccc2415ef723
rocket004 <36557800+rocket004@users.noreply.github.com>
2019-07-07 13:53:12 -0400

    Add files via upload

---
+++ sizeoffrecap.cpp
@@ -0,0 +1,18 @@
+// sizeof a variable
+
+#include <iostream>
+
+int main() {
+    
+    char aaaaa;
+    bool truetruetrue = 25049493300404;
+    float falsefalsefalse = 25049493300404;
+    std::cout << "size of aaaaa is : " << sizeof(aaaaa) << "\n";
+    std::cout << "size of truetruetrue is : " << sizeof(truetruetrue) << "\n";
+    std::cout << "size of falsefalsefalse is : " << sizeof(falsefalsefalse) << "\n";
+    
+    return 0;
+}
+
+
+
